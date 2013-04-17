5dde9c493b880dcd35318298118d99559037f529
Steve Purcell <steve@sanityinc.com>
2013-04-17 16:23:33 +0100

    Highlight non-.rb files created by `rails generate`

--- rinari.el
+++ rinari.el
@@ -277,7 +277,7 @@ Use `font-lock-add-keywords' in case of `ruby-mode' or
          (script (or script (jump-completing-read "Script: " completions)))
          (ruby-compilation-error-regexp-alist ;; for jumping to newly created files
           (if (equal script "generate")
-              '(("^ +\\(exists\\|create\\) +\\([^[:space:]]+\\.rb\\)" 2 3 nil 0 2))
+              '(("^ +\\(exists\\|create\\) +\\([^[:space:]]+\\)" 2 3 nil 0 2))
             ruby-compilation-error-regexp-alist))
          (script (concat (rinari--wrap-rails-command script) " ")))
     (ruby-compilation-run (concat script (read-from-minibuffer script)))))
